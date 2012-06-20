e28ffbebdf8af5af40aec8ac2a7a5ceefd3da37e
Kepeh Yoyo <Kepeh Yoyo>
2012-06-20 17:58:55 +0100

    editing

--- cookbooks/couchbase/recipes/default.rb
+++ cookbooks/couchbase/recipes/default.rb
@@ -20,12 +20,11 @@ end
 
  
 execute "s3cmd" do
-  command "get #{node[:couchbase][:bucket]}:#{node[:couchbase][:package]}  /tmp/couchbase_files"
-  environment ["AWS_ACCESS_KEY_ID"   => node[:aws][:access_key_id],
-              "AWS_SECRET_ACCESS_KEY" => node[:aws][:secret_access_key],
-              "AWS_CALLING_FORMAT"   => "SUBDOMAIN"]
-  action :run
-end
+  command "s3cmd get #{node[:couchbase][:bucket]}:#{node[:couchbase][:package]}  /tmp/couchbase_files"
+    environment ({ 'AWS_ACCESS_KEY_ID'  => node[:aws][:access_key_id] , 
+    'AWS_SECRET_ACCESS_KEY' => node[:aws][:secret_access_key] ,
+    'AWS_CALLING_FORMAT'  => 'SUBDOMAIN'})
+end  
 
 execute "tar" do
   command  " tar -xzf /tmp/couchbase_files/#{node[:couchbase][:package]}"  
