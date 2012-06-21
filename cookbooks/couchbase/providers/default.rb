365024b384523f806e04c1b6032f0c06faff5f8a
Kepeh Yoyo <Kepeh Yoyo>
2012-06-21 21:25:04 +0100

    couchbase update

--- cookbooks/couchbase/providers/default.rb
+++ cookbooks/couchbase/providers/default.rb
@@ -14,6 +14,7 @@ action :install do
 end
 
 action :join_cluser do
-
-
+    execute "curl" do
+      command "curl  -u #{new_resource.user}:#{new_resource.password}  -d clusterMemberPort=8091  -d user=#{new_resource.user}  -d password=#{new_resource.password}  http://localhost:8091/node/controller/doJoinCluster 2> /dev/null"
+    end
 end
--- cookbooks/couchbase/recipes/join_cluster.rb
+++ cookbooks/couchbase/recipes/join_cluster.rb
@@ -1,5 +1,14 @@
 
 if
-  if  node[:couchbase][:cluster_address] 
+   if  node[:couchbase][:password] &&  node[:couchbase][:user]
+    if  node[:couchbase][:cluster_address] 
+  
+        couchbase "add to cluster" do
+           user            node[:couchbase][:user]
+           password        node[:couchbase][:password]
+           cluster_address node[:couchbase][:cluster_address]
+           action          :join_cluster
+     end
+    end
   end
 end
